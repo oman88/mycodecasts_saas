a846321b86c3a56f8f218753854c44b0c7c8da84
Orestes Chinea <ochinea@hotmail.com>
2015-07-11 03:47:16 +0000

    fixing contacts.rb

--- app/controllers/concerns/contacts_controller.rb
+++ app/controllers/concerns/contacts_controller.rb
@@ -1,7 +1,17 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
   
   def create
+    @contact = Contact.build(contact_params)
   end
 end
+
+# define your contact params here
+
+private
+
+ def contact_params
+   params.require(:contact).permit(:name, :email, :comments)
+ end
